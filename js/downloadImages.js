e9b82b9498077277eb6ff7d7f45fcd7aec752f17
lleellee0 <lleellee013@gmail.com>
2019-03-03 17:56:19 +0900

    다음의 선택자, URL로 변경 // 현재 웹툰의 각 화에 대한 접근은 하고 있으나 이미지 다운로드 안되고 있음.

--- js/downloadImages.js
+++ js/downloadImages.js
@@ -19,27 +19,28 @@ const downloadWebToonImage = (uri, no, downloadPath, startNumber, endNumber, i, 
     request({
       uri:uri,
       headers: {
-        accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        acceptEncoding:'gzip, deflate, sdch',
-        acceptLanguage:'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+        accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        acceptEncoding:'gzip, deflate',
+        acceptLanguage:'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,da;q=0.6',
         connection:'keep-alive',
         /* host:'imgcomic.naver.net', 
             This line has next ISSUE Error [ERR_TLS_CERT_ALTNAME_INVALID]: 
             Hostname/IP does not match certificate's altnames: Host: imgcomic.naver.net. is not in the cert's altnames: DNS:*.pstatic.net, DNS:pstatic.net
         */
-        referer:uri + startNumber +'.jpg',
+        // referer:uri + startNumber +'.jpg',
         upgradeInsecureRequests:'1',
-        userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+        userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
       },
       encoding: 'binary'
     }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         fs.writeFile(`${downloadPath}\\${paddingNumber("0000", no)}-${paddingNumber("000", i)}.jpg`, body, 'binary', (err) => {if(err) console.log(err)});
       } else {
+        if(retryCount )
         console.log("Connection Timeout. " + no + " " + i + " 재시도 합니다. 남은 재시도 횟수 : 3");
-        log.addErrorLog(`${titleId}의 ${no}화에서 ${i}번째 그림을 다운로드하다가 실패했습니다. 남은 재시도 횟수 : 3`);
-
-        retryDownloadWebToonImages(uri, no, downloadPath, startNumber, endNumber, i, 3);
+        log.addErrorLog(`${titleId}의 ${no}화에서 ${i}번째 그림을 다운로드하다가 실패했습니다. 남은 재시도 횟수 : ${retryCount}`);
+          if(retryCount > 0)
+        downloadWebToonImages(uri, no, downloadPath, startNumber, endNumber, i, retryCount--);
 
         console.log(error);
       }
